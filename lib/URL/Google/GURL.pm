aeb06c4ea129efe8c54b5644ead62641b8dcbb0c
Michael Ellery <mellery@yahoo.com>
2011-12-16 09:08:38 -0800

    first version

---
+++ lib/URL/Google/GURL.pm
@@ -0,0 +1,29 @@
+package URL::Google::GURL;
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+require XSLoader;
+XSLoader::load('URL::Google::GURL', $VERSION);
+
+1;
+__END__
+
+=head1 NAME
+
+URL::Google::GURL -  Google url library, GURL partial export
+
+=head1 DESCRIPTION
+
+TBD
+
+=head1 AUTHOR
+
+Mike Ellery, E<lt>mellery@yahoo.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+
+=cut
